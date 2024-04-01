aa362fa96aacc92b3ad756545a8730cdd4ec8bb1
Mahesh Kokate <mahesh.g.kokate@gmail.in>
2024-04-01 15:23:28 +0530

    add locomotive

--- animation.js
+++ animation.js
@@ -1,3 +1,8 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
 function navAnimation(){
     document.querySelector("nav").addEventListener("mouseenter",function(){
         let timeline = gsap.timeline();
@@ -98,6 +103,52 @@ function pageThreeVideoAnimation(){
 
 }
 
+function locomotive(){
+
+    gsap.registerPlugin(ScrollTrigger);
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+
+  // for tablet smooth
+  tablet: { smooth: true },
+
+  // for mobile
+  smartphone: { smooth: true }
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length
+      ? locoScroll.scrollTo(value, 0, 0)
+      : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  }
+
+  // follwoing line is not required to work pinning on touch screen
+
+  /* pinType: document.querySelector("#main").style.transform
+    ? "transform"
+    : "fixed"*/
+});
+
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+
+}
+
+
 if(window.innerWidth < 750){
     navAnimation();
 }
@@ -106,6 +157,7 @@ pageTwoAnimation();
 
 pageThreeVideoAnimation();
 
+locomotive();
 
 
 
